d6b0f4b6642b4752cbff7fd97f597428b14f997a
tanveer2382 <tanveer2382@gmail.com>
2024-05-20 18:24:54 +0500

    First
    First Upload

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { quiz } from './assets/data/gkquestions'
+import './index.css'
+
+//function App(){}
+const App = () => {
+
+
+
+  const [activeQuestion, setActiveQuestion] = useState(0)
+  const [selectedAnswer, setSelectedAnswer] = useState('')
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null)
+  const [result, setResult] = useState({
+    score: 0,
+    corectAnswers: 0,
+    wrongAnswers: 0
+  });
+
+
+  const { questions } = quiz;
+  const { question, choices, correctAnswer } = questions[activeQuestion]
+
+  const onClickNext = () => {
+    setSelectedAnswerIndex(null)
+    setActiveQuestion((prev) => prev + 1)
+    setResult((prev) => selectedAnswer
+      ? {
+        ...prev,
+        score: prev.score + 5,
+        corectAnswers: prev.corectAnswers + 1
+
+      }
+      :
+      {
+         ...prev,
+        wrongAnswers: prev.wrongAnswers + 1
+      }
+    )
+  }
+
+  const onAnswerSelected = (answer, index) => {
+    setSelectedAnswerIndex(index)
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true)
+      console.log('right' + index)
+    }
+    else {
+      setSelectedAnswer(false)
+      console.log('wrong' + index)
+    }
+  }
+
+  return (
+    <>
+      <div className="card">
+        <h1>Quiz App</h1>
+        {/* <h2>{questions[activeQuestion].question}</h2> */}
+        <h2>{question}</h2>
+        <ul>
+          {
+            choices.map((item, index) => (
+              <li onClick={() => onAnswerSelected(item, index)} key={item}
+                // className={selectedAnswerIndex === index ? 'selected-answer' : null}
+                className={selectedAnswer && selectedAnswerIndex === index ? 'rigth-answer' : selectedAnswerIndex === index ? 'wrong-answer' : null}
+
+              >
+                {index}{item}
+              </li>
+            ))
+          }
+        </ul>
+        <button onClick={onClickNext} disabled={selectedAnswerIndex ===null}>
+          {activeQuestion === questions.length - 1 ? "Finish" : "Next"}
+        </button>
+
+        
+          <h1>Score:{result.score}</h1>
+          <h1>Correct Answers:{result.corectAnswers}</h1>
+          <h1>Wrong Answers:{result.wrongAnswers}</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/data/gkquestions.js
@@ -0,0 +1,36 @@
+export const quiz = {
+    topic : "javascript",
+    level: "beginners",
+    totalQuestions: 4,
+    perQuestionScore: 5,
+    questions:[
+      {
+        question: "1. which function is used to serialize an object into JSON string in javasctipt?",
+        choices: ["stringyfy()", "pasrse()", "convert()", "None of the Above"],
+        type: "MCQs",
+        correctAnswer: "stringyfy()",
+      },
+    
+      {
+        question: "2. which of the following is used to define a javascript variable?",
+        choices: ["var", "let", "var and let", "None of the Above"],
+        type: "MCQs",
+        correctAnswer: "var and let",
+      },
+    
+      {
+        question: "3.which of the following is used to define a data in some format from using javascript?",
+        choices: ["document.write()", "console.log()", "window.alert()", "All of the Above"],
+        type: "MCQs",
+        correctAnswer: "All of the Above",
+      },
+      {
+        question:"4.which of the following is used to define a excel in some format from using javascript?",
+        choices: ["hi", "pagal", "dushman", "All of the Above"],
+        type: "MCQs",
+        correctAnswer: "hi",
+      },
+    ]
+    };
+
+    
